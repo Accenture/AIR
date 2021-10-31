6e333d6df76d1d38644791265ef8b9cb5478ec3e
Pasi Pyrrö <pasi.pyrro@accenture.com>
2021-10-31 14:11:14 +0200

    add experimental nvidia-tensorflow environment that supports the latest NVIDIA A100 GPUs

---
+++ start-air-gpu-experimental-env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+  echo 'Usage: bash '$(basename $0)' [CONTAINER_TYPE="docker"]'
+  exit 0
+fi
+
+CONTAINER_TYPE="docker"
+
+CONTAINER_TYPE="${1:-$CONTAINER_TYPE}"
+if [[ "$CONTAINER_TYPE" == "docker" ]]; then
+    docker run --gpus all --env WANDB_API_KEY=$WANDB_API_KEY -it --network host -v $PWD:/home -w /home intrinsick/keras-retinanet-gpu:nvidia-tf /bin/bash
+elif [[ "$CONTAINER_TYPE" == "singularity" ]]; then
+    # YOU MIGHT WANNA CHANGE WHERE SINGULARITY STORES ITS CACHES (THEY TAKE UP A LOT OF SPACE)
+    # IT'S LIKELY NOT A GOOD IDEA TO STORE THEM INSIDE THE MOUNTED VOLUME EITHER
+    export SINGULARITY_CACHEDIR=$PWD/../.singularity
+    # singularity build keras-retinanet-gpu.simg docker://intrinsick/keras-retinanet-gpu:nvidia-tf
+    singularity shell -B $PWD:/home/$USER/AIR --nv docker://intrinsick/keras-retinanet-gpu:nvidia-tf
+else
+    echo "Invalid container type: ${CONTAINER_TYPE}"
+fi
